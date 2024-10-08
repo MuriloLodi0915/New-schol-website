6ab40fa97a2ce67eae117fd716742408f4ae2e21
MuriloLodi0915 <157790354+MuriloLodi0915@users.noreply.github.com>
2024-10-08 04:06:01 -0300

    v2

---
+++ Assets/js/parallaxscroll.js
@@ -0,0 +1,27 @@
+const page2Image = document.querySelector('.page2-image');
+const page2ImageContainer = document.querySelector('.page2-image-container');
+
+// Define o fator de velocidade
+const speedFactor = 0.25; // Ajuste o fator de velocidade (menor que 1 para mais lento)
+const startEffectOffset = -500; // Ajuste esta variável para definir quando o efeito começa (em pixels)
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY + Math.abs(startEffectOffset); // Adiciona o offset negativo
+
+  const containerOffsetTop = page2ImageContainer.offsetTop;
+  const containerHeight = page2ImageContainer.offsetHeight;
+  const imageHeight = page2Image.offsetHeight;
+
+  const maxScroll = imageHeight - containerHeight;
+
+  // Verifica se a rolagem está dentro dos limites do contêiner
+  if (scrollPosition > containerOffsetTop) {
+    // Ajusta o efeito de paralaxe
+    const adjustedScroll = (scrollPosition - containerOffsetTop) * speedFactor;
+
+    // Impede que a imagem se mova além do limite do contêiner
+    const translateY = Math.min(adjustedScroll, maxScroll);
+    
+    page2Image.style.transform = `translateY(-${translateY}px)`;
+  }
+});
